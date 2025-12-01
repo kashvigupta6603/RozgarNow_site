6c80299b9989277c29957d4391b1651a654d66f3
kashvigupta6603 <kashvigupta1612@gmail.com>
2025-12-02 02:10:33 +0530

    Remove bun lock and force npm

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true, // if cookies auth required
 });
 
-export const register = (data) => API.post("/auth/register", data);
-export const login = (data) => API.post("/auth/login", data);
+export default API;
